ad756e536d86a04e445bbcbd3b8ede60cd5966be
ZylalMinollari <zmiollari@gmail.com>
2023-08-08 17:31:17 +0200

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,231 @@
+import {
+  backend,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    title: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    title: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    title: "Lorem Ipsum",
+    icon: backend,
+  },
+];
+
+const technologies = [
+  {
+    name: "Lorem Ipsum ",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+  {
+    name: "Lorem Ipsum",
+    icon: backend,
+  },
+];
+
+const experiences = [
+  {
+    title: "Lorem Ipsum",
+    company_name: "Lorem Ipsum",
+    icon: backend,
+    iconBg: "#383E56",
+    date: "Lorem Ipsum",
+    points: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non sollicitudin lacus, sit amet lacinia velit. ",
+      "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+      "Aliquam erat volutpat. Pellentesque eget odio et risus interdum tincidunt.",
+      "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    ],
+  },
+  {
+    title: "Lorem Ipsum",
+    company_name: "Lorem Ipsum",
+    icon: backend,
+    iconBg: "#E6DEDD",
+    date: "Lorem Ipsum",
+    points: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non sollicitudin lacus, sit amet lacinia velit. ",
+      "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+      "Aliquam erat volutpat. Pellentesque eget odio et risus interdum tincidunt.",
+      "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    ],
+  },
+  {
+    title: "Lorem Ipsum",
+    company_name: "Lorem Ipsum",
+    icon: backend,
+    iconBg: "#383E56",
+    date: "Lorem Ipsum",
+    points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non sollicitudin lacus, sit amet lacinia velit. ",
+        "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+        "Aliquam erat volutpat. Pellentesque eget odio et risus interdum tincidunt.",
+        "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    ],
+  },
+  {
+    title: "Lorem Ipsum",
+    company_name: "Lorem Ipsum",
+    icon: backend,
+    iconBg: "#E6DEDD",
+    date: "Lorem Ipsum",
+    points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non sollicitudin lacus, sit amet lacinia velit. ",
+        "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+        "Aliquam erat volutpat. Pellentesque eget odio et risus interdum tincidunt.",
+        "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    name: "Lorem",
+    designation: "Lorem",
+    company: "Lorem",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "Quisque sem risus, porta quis diam suscipit, dapibus accumsan elit.",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Lorem Ipsum",
+    description:
+      "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+    tags: [
+      {
+        name: "lorem",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "lorem",
+        color: "green-text-gradient",
+      },
+      {
+        name: "lorem",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: backend,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Lorem Ipsum",
+    description:
+      "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+    tags: [
+      {
+        name: "lorem",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "lorem",
+        color: "green-text-gradient",
+      },
+      {
+        name: "lorem",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: backend,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Lorem Ipsum",
+    description:
+      "Nam efficitur convallis felis, nec fermentum nulla semper id. Maecenas non lorem sed nisi tincidunt dapibus pretium ut dui.",
+    tags: [
+      {
+        name: "lorem",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "lorem",
+        color: "green-text-gradient",
+      },
+      {
+        name: "lorem",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: backend,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
