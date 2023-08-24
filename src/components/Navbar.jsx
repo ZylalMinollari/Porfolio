6c6d0b06bb01a92695db18f330c6afc95fbcd61b
ZylalMinollari <zmiollari@gmail.com>
2023-08-24 14:51:59 +0200

    Fix in logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { zlogo, menu, close } from "../assets";
+import { logo, menu, close } from "../assets";
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
@@ -38,7 +38,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={zlogo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             ZYLAL &nbsp;
             <span className="sm:block hidden">| Minollari</span>
